2106f3aae586e574b7b0504c656528d63d7b51b1
Liyaz <istrebitelone@gmail.com>
2022-11-15 19:50:25 +1000

    pagenation fix issue

--- src/app/components/angebote/angebote.component.ts
+++ src/app/components/angebote/angebote.component.ts
@@ -2,30 +2,46 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiService } from 'src/app/services/api.service';
+import { isBigIntLiteral } from 'typescript';
 
 @Component({
   selector: 'app-angebote',
   templateUrl: './angebote.component.html',
   styleUrls: ['./angebote.component.css'],
 })
+
 export class AngeboteComponent implements OnInit {
-  angebote$ = new Observable<Angebot[]>();
-  stop=false
-  data:Angebot[] =[]
-  constructor(private apiService: ApiService, private httpClient: HttpClient) {}
 
-  public currentPageNum :any = "20";
+    angebote$ = new Observable<Angebot[]>();
+    stop = false
+    data: Angebot[] =[]
+    original: Angebot[] =[];
 
-  ngOnInit(): void {
-    window.scrollTo(0,0)
-    this.angebote$ = this.apiService.getAngebote();
+    constructor(private apiService: ApiService, private httpClient: HttpClient) {}
 
-    setTimeout(() => {
-      this.stop=true
-    }, 1000);
-  }
+    public length  : number  = 0;
+    public pageSize : number = 5;
+    public pageSizeOptions: any = [5, 10, 30];
 
-  show(e: Hits) {
-   this.data = (<Angebot[]>(<unknown>e)).reverse()
-  }
+    ngOnInit(): void {
+        window.scrollTo(0, 0);
+        this.angebote$ = this.apiService.getAngebote();
+        setTimeout(() => {
+            this.stop = true
+        }, 1000);
+    }
+
+    pageHandle(event: any){
+
+      const start: number = event.pageIndex * event.pageSize;
+      const end: number   = (event.pageIndex + 1) * event.pageSize;
+
+      this.data = this.original.slice(start, end);
+    }
+
+    show(e: Hits) {
+        this.original = (<Angebot[]>(<unknown>e)).reverse();
+        this.data   = this.original.slice(0, this.pageSize);
+        this.length = (<Angebot[]>(<unknown>e)).length;
+    }
 }
